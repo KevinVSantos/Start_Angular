048d6bf8f0e81d5500028c0d14da4085e19f4361
kevin.santos <kevin.santos@aluno.ifsp.edu.br>
2022-07-24 20:56:07 -0300

    Conclusão do curso

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Start_Angular
 Estudo sobre angular
+
+# Estudo sobre angular baseado no curso da Alura.
+Disponivel em: https://cursos.alura.com.br/course/angular-comecando-framework
+
+# Para iniciar o projeto, será necessário dois terminais:
+1. No primeiro navegue até a pasta dados e utilize o comando: json-server --watch db.json
+2. No segundo, no diretorio bytebank utilize o comando: ng s -o
--- bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,18 +1,45 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
+import { Router } from "@angular/router";
+import { TransferenciaService } from "../services/transferencia.service";
 
 @Component({
     selector:'app-nova-transferencia',
     templateUrl:'./nova-transferencia.component.html',
     styleUrls:['./nova-transferencia.component.scss']
 })
+
 export class NovaTransferencia{
+
+  @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number
   destino:number;
 
+  constructor(private service: TransferenciaService, private router: Router) {
+
+  }
+
   transferir(){
     console.log('Solicitação enviada');
-    console.log('Valor ', this.valor);
-    console.log('Destino ', this.destino);
+
+    this.service.adicionar({
+      valor: this.valor,
+      destino: this.destino
+    }).subscribe(
+      (resultado) => {
+        console.log(resultado);
+        this.limparCampos();
+        this.router.navigateByUrl('extrato');
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
   }
 }
